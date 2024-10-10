53fb0edb139a3dd19265cd3011e209f76330f5b7
kiitrr <renatuz10@gmail.com>
2024-10-10 20:19:00 +0500

    Update homework3.py

--- homework3/homework3.py
+++ homework3/homework3.py
@@ -1,3 +1,8 @@
+#Реализовать функцию, которая будет запрашивать дату рождения
+пользователя в формате день/месяц/год и выдавать его возраст в
+зависимости от сегодняшнего дня. 
+
+
 from datetime import datetime
 
 def calculate_age():
@@ -8,11 +13,14 @@ def calculate_age():
             break  
         except ValueError:
             print("Неверный формат даты. Пожалуйста, используйте формат дд/мм/гггг.")
+
     today = datetime.now()
     age = today.year - birth_date.year
 
-    
+
     if (today.month, today.day) < (birth_date.month, birth_date.day):
         age -= 1  
 
+    print(f"Ваш возраст: {age} лет.")
 
+calculate_age()
