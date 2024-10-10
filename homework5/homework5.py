3d75419fa517f1a7ca64b9802d191d76c61491d5
kiitrr <renatuz10@gmail.com>
2024-10-10 20:22:05 +0500

    Update homework5.py

--- homework5/homework5.py
+++ homework5/homework5.py
@@ -1,9 +1,23 @@
+#Определите функцию генератора get_number(), которая
+возвращает нечетные числа из диапазона range(30). Используйте цикл for,
+чтобы найти и вывести первое, пятое и последнее возращенной значение.
+
 def get_number():
     for num in range(30):
-        if num % 2 != 0:  
+        if num % 2 != 0: 
             yield num
 
 def main():
     odd_numbers = list(get_number()) 
 
-    
+   
+    if len(odd_numbers) < 5:
+        print("Недостаточно нечетных чисел в диапазоне.")
+        return
+
+    print("Первое нечетное число:", odd_numbers[0])
+    print("Пятое нечетное число:", odd_numbers[4])
+    print("Последнее нечетное число:", odd_numbers[-1])
+
+
+main()
