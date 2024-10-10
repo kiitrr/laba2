068e4edc4aed6f254e94536cacc401f32d091067
kiitrr <renatuz10@gmail.com>
2024-10-10 19:56:40 +0500

    Create homework3.py

---
+++ homework3/homework3.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+def calculate_age():
+    while True:
+        birth_date_str = input("Введите дату рождения (дд/мм/гггг): ")
+        try:
+            birth_date = datetime.strptime(birth_date_str, '%d/%m/%Y')
+            break  
+        except ValueError:
+            print("Неверный формат даты. Пожалуйста, используйте формат дд/мм/гггг.")
+
+    today = datetime.now()
+    age = today.year - birth_date.year
+
+    
+    if (today.month, today.day) < (birth_date.month, birth_date.day):
+        age -= 1  
+
+    print(f"Ваш возраст: {age} лет.")
+
+
+calculate_age()
